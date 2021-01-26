2fc3386e9d6e5c2c3ba0ecef7b17d967229879d5
Dasha <dasha.ogulia1994@gmail.com>
2021-01-26 13:42:54 +0200

    added tabs

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 
-
+//menu mobile
 let mobileMenu = document.querySelector(".nav-mobile-menu");
 let mainMenu = document.querySelector(".menu");
 
@@ -27,3 +27,39 @@ menu.addEventListener("click", function(){
     }
 })
 
+//tabs
+
+let tabItems = document.querySelectorAll(".tab-title-item");
+let tabContent = document.querySelectorAll(".tab-content");
+let tabWrap = document.querySelector(".tab-title-items");
+
+function showTabsContent(i=0){
+    tabContent[i].classList.add("show");
+    tabContent[i].classList.remove("hide");
+    tabItems[i].classList.add("active");
+}
+
+
+function hideTabsContent(){
+    tabContent.forEach(tab=>{
+        tab.classList.add("hide");
+        tab.classList.remove("show");
+    })
+    tabItems.forEach(item =>{
+        item.classList.remove("active");
+    })
+}
+hideTabsContent()
+showTabsContent()
+
+tabWrap.addEventListener("click", function(e){
+    let target = e.target;
+    if(target.classList.contains("tab-title-item")){
+        tabItems.forEach((item,i)=>{
+            if(target == item){
+                hideTabsContent()
+                showTabsContent(i)
+            }
+        })
+    }
+})
